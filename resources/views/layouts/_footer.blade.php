6edd8eec42ce7e52bb62bae41a0ecbefc0c460a1
livephone <livephone1988@163.com>
2019-03-07 15:19:50 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+	<img class="brand-icon" src="https://www.easyicon.net/download/png/1231678/128/">
+	<a href="http://www.netoper.com" target=_blank>
+		熟能生巧
+	</a>
+	<div class="float-right">
+		<a href="/about">关于</a>
+	</div>
+</footer>
