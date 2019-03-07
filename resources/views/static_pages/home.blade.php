29b2f50589ae9a7b4879106564d9dbdc75d9f099
livephone <livephone1988@163.com>
2019-03-07 12:22:32 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+	<h1>Hello Geroge</h1>
+	<p class="lead">
+		您现在看到的是你自己写的<a href="http://www.netoper.com">主页</a> ，多丑都是自找的。
+	</p>
+	<p>
+		将就着看吧。
+	</p>
+	<p>
+		<a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+	</p>
+</div>
 @stop
